efaf4cc83d6a957a38f72062983f1e99fd6e4c08
Danny Meyer <danny.meyer@gmail.com>
2018-02-17 16:58:07 -0500

    Oscillator working correctly

--- Audio/Player/new_osc.py
+++ Audio/Player/new_osc.py
@@ -2,6 +2,7 @@ import threading
 import math
 import numpy as np
 import time
+import random
 import pyaudio
 
 
@@ -15,35 +16,85 @@ class SineOsc:
                              output=1)
 
         self.tau = math.pi * 2
-        self.chunk_size = 100
-        self.freq = 300
+        self.chunk_size = 400
+        self.freq = 0.0
         self.phase = 0.0
+        self.last_freq = 0
 
-        thread = threading.Thread(target=self.update_freq, args=())
+        self.fade_in = np.arange(0., 1., 1./self.chunk_size)
+        self.fade_out = np.arange(1., 0., -1./self.chunk_size)
+
+        thread = threading.Thread(target=self.oscillator)
         thread.daemon = True
         thread.start()
 
-    def update_phase(self):
-        self.phase += self.freq * self.chunk_size * self.tau / self.rate
+    def update_phase(self, freq):
+        self.phase += freq * self.chunk_size * self.tau / self.rate
         self.phase %= self.tau
 
-    def make_chunk(self):
-        phase = (np.arange(self.chunk_size) * (self.freq * self.tau / self.rate)) + self.phase
+    def make_chunk(self, freq):
+        if freq == 0:
+            chunk = np.zeros(self.chunk_size)
+            return chunk
+        phase = (np.arange(self.chunk_size) * (freq * self.tau / self.rate)) + self.phase
         phase %= self.tau
-        waveform = np.sin(phase)
-        return waveform
+        chunk = np.sin(phase) / 10
+        return chunk
 
-    def write(self):
-        chunk = self.make_chunk()
-        self.update_phase()
+    def portamento(self):
+        diff = self.last_freq - self.freq
+        if diff > 0:
+            while self.last_freq > self.freq:
+                chunk = self.make_chunk(self.last_freq)
+                self.write_to_stream(chunk, self.last_freq)
+                self.last_freq -= 40
+        else:
+            while self.last_freq < self.freq:
+                chunk = self.make_chunk(self.last_freq)
+                self.write_to_stream(chunk, self.last_freq)
+                self.last_freq += 40
+
+    def write_attack(self):
+        chunk = self.make_chunk(self.freq)
+        chunk[:self.chunk_size] = np.multiply(chunk[:self.chunk_size], self.fade_in)
+        self.write_to_stream(chunk, self.freq)
+
+    def write_decay(self):
+        chunk = self.make_chunk(self.last_freq)
+        chunk[-self.chunk_size:] = np.multiply(chunk[-self.chunk_size:], self.fade_out)
+        self.write_to_stream(chunk, 0)
+
+    def manage_transition(self):
+        if self.last_freq != self.freq:
+            if self.freq == 0:
+                self.write_decay()
+            elif self.last_freq == 0:
+                self.write_attack()
+            else:
+                self.portamento()
+        self.last_freq = self.freq
+
+    def write_to_stream(self, chunk, freq):
+        self.update_phase(freq)
         self.stream.write(chunk.astype(np.float32).tostring())
 
-    def update_freq(self):
+    def run(self):
+        self.manage_transition()
+        chunk = self.make_chunk(self.freq)
+        self.write_to_stream(chunk, self.freq)
+
+    def oscillator(self):
         while True:
-            self.write()
+            self.run()
 
 osc = SineOsc()
 
-while True:
-    osc.freq += 1
-    time.sleep(.01)
+if __name__ == "__main__":
+    print('Oscillator test...')
+    while True:
+        rand = random.randint(0, 2)
+        if rand == 0:
+            osc.freq = 0
+        else:
+            osc.freq = random.randint(100, 400)
+        time.sleep(.5)
