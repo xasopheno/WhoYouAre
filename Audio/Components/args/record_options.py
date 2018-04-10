49c0bc4a4ab5c2e9ea362eb0ff1989af033950f1
Danny <danny.meyer@gmail.com>
2018-04-10 15:04:57 -0400

    improves args, adds gitignore, adds trained models

--- Audio/Components/args/record_options.py
+++ Audio/Components/args/record_options.py
@@ -7,68 +7,66 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.par
 def record_options():
     a = argparse.ArgumentParser()
 
-    a.add_argument("--display_volume",
-                   help = "Specify if input volume should be displayed.",
-                   dest = "display_volume",
+    a.add_argument("-vol",
+                   action='store_true',
+                   help="Specify if input volume should be displayed.",
+                   dest="display_volume",
                    required=False,
-                   default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
-    a.add_argument("--display_predictions",
+    a.add_argument("-val",
                    help="Specify if prediction values should be displayed.",
+                   action='store_true',
                    dest = "display_prediction",
                    required=False,
-                   default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
-    a.add_argument("--write_csv",
+    a.add_argument("-csv",
                    help="Specify if a csv should be written.",
+                   action='store_true',
                    dest="write_csv",
                    required=False,
                    default=False,
-                   type=str,
-                   nargs=1)
+                   )
 
-    a.add_argument("--play_midi",
+    a.add_argument("-midi",
                    help="Specify if midi should be sent with python-rt-midi.",
+                   action='store_true',
                    dest="play_midi",
                    required=False,
                    default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
-    a.add_argument("--play_websocket",
+    a.add_argument("-socket",
                    help="Specify if frequencies should be sent to a local websocket.",
+                   action='store_true',
                    dest="play_websocket",
                    required=False,
                    default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
-    a.add_argument("--filtered",
+    a.add_argument("-filtered",
                    help="filter out redundant notes).",
+                   action='store_true',
                    dest="filtered",
                    required=False,
                    default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
-    a.add_argument("--py_osc",
+    a.add_argument("-py_osc",
                    help="Specify if frequencies should be sent to the python oscillator.",
+                   action='store_true',
                    dest="wave",
                    required=False,
                    default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
-    a.add_argument("--nn",
+    a.add_argument("-nn",
                    help="Improvise with neural network.",
+                   action='store_true',
                    dest="nn",
                    required=False,
                    default=False,
-                   type=bool,
-                   nargs=1)
+                   )
 
     return a.parse_args()
